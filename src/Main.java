b6417cd635ee3834e86f1667cbe5278a67a2267d
黄乐豪 <huang.lehao@hand-china.com>
2020-06-17 07:11:55 +0800

    合并后的123

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,8 @@ public class Main {
         System.out.println("张三 third commit1");
         System.out.println("张三 third commit2");
         System.out.println("张三 third commit3");
+        System.out.println("1111111");
+        System.out.println("2222222");
+        System.out.println("3333333");
     }
 }
