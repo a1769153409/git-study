13bb1d1d3106e8a8aacf662bb5414f518eeffb67
黄乐豪 <huang.lehao@hand-china.com>
2020-06-17 07:43:28 +0800

    [add&update]新增test文件并修改main

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("1111111");
         System.out.println("2222222");
         System.out.println("3333333");
+        System.out.println("444444444")
     }
 }
