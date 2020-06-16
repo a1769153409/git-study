3712d43f64a082ef98c59d44ee2fa70d88a1e313
黄乐豪 <huang.lehao@hand-china.com>
2020-06-17 07:00:36 +0800

    合并后的commit3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("张三 third commit3");
+        System.out.println("张三 third commit1");
+        System.out.println("张三 third commit2");
+        System.out.println("张三 third commit3");
     }
 }
