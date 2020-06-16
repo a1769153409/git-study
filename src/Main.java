75ae3199ff31424b59e4bb6f253bb8b8c4416929
黄乐豪 <huang.lehao@hand-china.com>
2020-06-17 07:48:45 +0800

    [add&update]修改main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("张三 third commit3");
-        System.out.println("张三 third commit1");
-        System.out.println("张三 third commit2");
-        System.out.println("张三 third commit3");
-        System.out.println("1111111");
-        System.out.println("2222222");
-        System.out.println("3333333");
-        System.out.println("444444444")
+        System.out.println("张三 third commit");
     }
 }
