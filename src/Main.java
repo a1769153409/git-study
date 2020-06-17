aba451fa46c5a2ac402dcab8ff8565088b48d933
黄乐豪 <huang.lehao@hand-china.com>
2020-06-18 06:15:33 +0800

    [update]hhhhhy

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("张三 third commit");
+        System.out.println("张三 third commit");
+        System.out.println("hhhhhy")
     }
 }
